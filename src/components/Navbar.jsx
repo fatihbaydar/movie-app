46e67b84f6031696633a47d683fdf76117fb8bf6
fatihbaydar <fatihbaydar2004@gmail.com>
2024-10-10 16:47:50 +0300

    navbar edited

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,14 +8,13 @@ import {
 
 import { Link } from "react-router-dom";
 import SwitchButton from "./SwitchButton";
-
-function classNames(...classes) {
-  return classes.filter(Boolean).join(" ");
-}
+import avatar from "../assets/icons/avatar.png";
 
 export default function Navbar() {
-  const currentUser = {displayName: "Baydar"}
+  const currentUser = { displayName: "Baydar" };
   return (
+    <>
+   
     <Disclosure
       as="nav"
       className="bg-neutral-100 dark:bg-gray-900 dark:text-white fixed top-0 w-full z-3"
@@ -27,20 +26,19 @@ export default function Navbar() {
           </Link>
 
           <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
-          {currentUser && (
-                <h5 className="mr-2 capitalize ">
-                  {currentUser?.displayName}
-                </h5>)}
-                <SwitchButton/>
+            {currentUser && (
+              <h5 className="mr-2 capitalize ">{currentUser?.displayName}</h5>
+            )}
+            <SwitchButton />
             {/* Profile dropdown */}
-            <Menu as="div" className="relative ml-3">
+            <Menu as="div" className="relative">
               <div>
                 <MenuButton className="relative flex rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
                   <span className="absolute -inset-1.5" />
                   <span className="sr-only">Open user menu</span>
                   <img
                     alt=""
-                    src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+                    src={currentUser?.photoURL || avatar}
                     className="h-8 w-8 rounded-full"
                   />
                 </MenuButton>
@@ -50,28 +48,25 @@ export default function Navbar() {
                 className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 transition focus:outline-none data-[closed]:scale-95 data-[closed]:transform data-[closed]:opacity-0 data-[enter]:duration-100 data-[leave]:duration-75 data-[enter]:ease-out data-[leave]:ease-in"
               >
                 <MenuItem>
-                  <a
-                    href="#"
+                  <Link
+                    to="/register"
                     className="block px-4 py-2 text-sm text-gray-700 data-[focus]:bg-gray-100"
                   >
-                    Your Profile
-                  </a>
+                    Register
+                  </Link>
                 </MenuItem>
                 <MenuItem>
-                  <a
-                    href="#"
+                  <Link
+                    to="/login"
                     className="block px-4 py-2 text-sm text-gray-700 data-[focus]:bg-gray-100"
                   >
-                    Settings
-                  </a>
+                    Login
+                  </Link>
                 </MenuItem>
                 <MenuItem>
-                  <a
-                    href="#"
-                    className="block px-4 py-2 text-sm text-gray-700 data-[focus]:bg-gray-100"
-                  >
-                    Sign out
-                  </a>
+                  <span className="block px-4 py-2 text-sm text-gray-700 data-[focus]:bg-gray-100 cursor-pointer">
+                    Log out
+                  </span>
                 </MenuItem>
               </MenuItems>
             </Menu>
@@ -79,5 +74,7 @@ export default function Navbar() {
         </div>
       </div>
     </Disclosure>
+    <div className="h-16"></div>
+    </>
   );
 }
