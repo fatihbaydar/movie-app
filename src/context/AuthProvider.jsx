db44d17ce6430cfaff38457bdc1381735935e3ac
fatihbaydar <fatihbaydar2004@gmail.com>
2024-10-11 16:09:11 +0300

    sign in

--- src/context/AuthProvider.jsx
+++ src/context/AuthProvider.jsx
@@ -1,6 +1,9 @@
-import { createUserWithEmailAndPassword } from "firebase/auth";
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+} from "firebase/auth";
 import React, { createContext, useContext, useState } from "react";
-import {auth} from "../auth/firebase"
+import { auth } from "../auth/firebase";
 import { useNavigate } from "react-router-dom";
 import { toastErrorNotify, toastSuccessNotify } from "../helper/ToastNotify";
 
@@ -12,22 +15,45 @@ export const useAuthContext = () => {
 };
 
 const AuthProvider = ({ children }) => {
-    const [currentUser, setCurrentUser] = useState(false)
-    const navigate = useNavigate()
+  const [currentUser, setCurrentUser] = useState(false);
+  const navigate = useNavigate();
 
-    const createUser = async(email,password) => {
-        try {
-           let userCredential = await createUserWithEmailAndPassword(auth,email,password)
-        //    console.log(userCredential);
-           navigate("/login")
-           toastSuccessNotify("Registered successfully")
-        } catch (error) {
-            // console.log(error)
-            toastErrorNotify(error.message)
-        }
+  const createUser = async (email, password) => {
+    try {
+      let userCredential = await createUserWithEmailAndPassword(
+        auth,
+        email,
+        password
+      );
+      //    console.log(userCredential);
+      navigate("/login");
+      toastSuccessNotify("Registered successfully");
+    } catch (error) {
+      // console.log(error)
+      toastErrorNotify(error.message);
     }
+  };
 
-    const values = {currentUser, createUser};
+  //* https://console.firebase.google.com/
+  //* => Authentication => sign-in-method => enable Email/password
+  //! Email/password ile girişi enable yap
+
+  const signIn = async (email, password) => {
+    try {
+      let userCredential = await signInWithEmailAndPassword(
+        auth,
+        email,
+        password
+      );
+      //    console.log(userCredential);
+      navigate("/");
+      toastSuccessNotify("Logged in successfully");
+    } catch (error) {
+      // console.log(error)
+      toastErrorNotify(error.message);
+    }
+  };
+  const values = { currentUser, createUser, signIn };
   return <AuthContext.Provider value={values}>{children}</AuthContext.Provider>;
 };
 
