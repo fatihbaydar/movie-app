dbe0edc17bef5157d475391745c8561719ce32ff
fatihbaydar <fatihbaydar2004@gmail.com>
2024-10-16 21:40:46 +0300

    colored vote_average

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,20 +1,25 @@
 import React from "react";
 import { useMovieContext } from "../context/MovieProvider";
 import MovieCard from "../components/MovieCard";
+import Loading from "../components/Loading";
 
 const API_KEY = process.env.REACT_APP_TMDB_KEY;
 const SEARCH_API = `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&query=`;
 
 const Main = () => {
-  const {movies, loading} = useMovieContext()
-  console.log(movies)
-  return <>
-  <div className="flex flex-wrap justify-center">
-    {movies.map((movie) => (
-      <MovieCard key={movie.id} {...movie} />
-    ))}
-  </div>
-  </>;
+  const { movies, loading } = useMovieContext();
+  console.log(movies);
+  return (
+    <>
+      <div className="flex flex-wrap justify-center">
+        {loading ? (
+          <Loading />
+        ) : (
+          movies.map((movie) => <MovieCard key={movie.id} {...movie} />)
+        )}
+      </div>
+    </>
+  );
 };
 
 export default Main;
