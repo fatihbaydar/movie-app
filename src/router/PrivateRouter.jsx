9e5df563a1300be1fa54cba2c319a0289d441722
fatihbaydar <fatihbaydar2004@gmail.com>
2024-10-17 14:25:15 +0300

    PrivateRouter

---
+++ src/router/PrivateRouter.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { useAuthContext } from '../context/AuthProvider'
+import { Navigate, Outlet } from 'react-router-dom'
+
+const PrivateRouter = () => {
+    const {currentUser} = useAuthContext
+  return (
+  currentUser ? <Outlet/> : <Navigate  to="/login"/>
+  )
+}
+
+export default PrivateRouter
