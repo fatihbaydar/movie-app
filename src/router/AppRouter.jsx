7530d4b63d514820305f89e6dee1da32f5a8c1d3
fatihbaydar <fatihbaydar2004@gmail.com>
2024-10-17 17:35:12 +0300

    privateroter replace

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -5,17 +5,22 @@ import Login from "../pages/Login";
 import Register from "../pages/Register";
 import MovieDetail from "../pages/MovieDetail";
 import Navbar from "../components/Navbar";
+import PrivateRouter from "./PrivateRouter";
 
 const AppRouter = () => {
-  return <>
-  <Navbar/>
-  <Routes>
-    <Route path="/" element={<Main/>} />
-    <Route path="/login" element={<Login/>} />
-    <Route path="/register" element={<Register/>} />
-    <Route path="/details/:movieId" element={<MovieDetail/>} />
-  </Routes>
-  </>;
+  return (
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Main />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="" element={<PrivateRouter />}>
+          <Route path="/details/:movieId" element={<MovieDetail />} />
+        </Route>
+      </Routes>
+    </>
+  );
 };
 
 export default AppRouter;
